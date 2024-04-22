14c2efb81b1a1ba6ebb0a07e8e7cdbfe16aae20a
hameleonlab <hameleonlab@gmail.com>
2024-04-22 14:40:48 +0700

    simple code done

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <input type="text" 
+      v-model="simpleText">
+  </div>
+  <h1>{{ simpleText }}</h1>
 </template>
 
 <script>
 
 export default {
   name: 'App',
+  data() {
+    return {
+      simpleText: 'demo text'
+    }
+  },
 }
 </script>
 
